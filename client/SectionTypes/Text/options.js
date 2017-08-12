9f71f38faeda6a0eab09e3b9db12c974c1e8b395
ryanm <ryanmccombe@gmail.com>
2017-08-12 16:31:55 +0100

    Update Text Section Options

--- client/SectionTypes/Text/options.js
+++ client/SectionTypes/Text/options.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { Button, Label, Form } from 'semantic-ui-react';
+import { Button, Label, Form, Segment, Divider } from 'semantic-ui-react';
 
 import Slider from 'react-rangeslider';
 
@@ -27,42 +27,56 @@ class TextOptions extends PureComponent {
     const { onEdit, onClose, section: { currentContent } } = this.props;
 
     return (
-      <Form>
-        <Label>Alignment</Label>
-        <Form.Select
-          fluid
-          selection
-          defaultValue={currentContent.alignment}
-          onChange={(e, { value }) => onEdit('alignment', value)}
-          options={this.alignmentOptions}
-        />
+      <Segment>
+        <Form>
+          <Form.Field>
+            <label>Alignment</label>
+            <Form.Select
+              fluid
+              selection
+              defaultValue={currentContent.alignment}
+              onChange={(e, { value }) => onEdit('alignment', value)}
+              options={this.alignmentOptions}
+            />
+          </Form.Field>
 
-        <Label>Background Image</Label>
-        <Form.Input
-          placeholder={currentContent.backgroundImage}
-          onChange={e => onEdit('backgroundImage', e.target.value)}
-        />
+          <Divider />
 
-        <Label>Space Above Content</Label>
-        <Slider
-          value={currentContent.paddingTop}
-          min={1}
-          max={1200}
-          orientation="horizontal"
-          onChange={value => onEdit('paddingTop', value)}
-        />
+          <Form.Field>
+            <label>Background Image</label>
+            <Form.Input
+              placeholder={currentContent.backgroundImage}
+              onChange={e => onEdit('backgroundImage', e.target.value)}
+            />
+          </Form.Field>
 
-        <Label>Space Below Content</Label>
-        <Slider
-          value={currentContent.paddingBottom}
-          min={1}
-          max={1200}
-          orientation="horizontal"
-          onChange={val => onEdit('paddingBottom', val)}
-        />
+          <Divider />
 
-        <Button type="submit" onClick={onClose}>Close</Button>
-      </Form>
+          <Form.Field>
+            <label>Space Above Content</label>
+            <Slider
+              value={currentContent.paddingTop}
+              min={1}
+              max={1200}
+              orientation="horizontal"
+              onChange={value => onEdit('paddingTop', value)}
+            />
+          </Form.Field>
+
+          <Divider />
+
+          <Form.Field>
+            <label>Space Below Content</label>
+            <Slider
+              value={currentContent.paddingBottom}
+              min={1}
+              max={1200}
+              orientation="horizontal"
+              onChange={val => onEdit('paddingBottom', val)}
+            />
+          </Form.Field>
+        </Form>
+      </Segment>
     );
   }
 }
